c9a070ded7eb51a81d69d061fda3fbe58e71533f
BuildTools <unconfigured@null.spigotmc.org>
2022-02-07 14:53:06 -0600

    Buffed effect, removed amplifier based on level

--- src/main/java/ethans/vanilla/tweaks/ShulkersEssence.java
+++ src/main/java/ethans/vanilla/tweaks/ShulkersEssence.java
@@ -2,6 +2,7 @@ package ethans.vanilla.tweaks;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
@@ -22,11 +23,10 @@ public class ShulkersEssence extends Enchantment {
     }
     @Override
     public void onTargetDamaged(LivingEntity user, Entity target, int level) {
-        if(target instanceof LivingEntity) {
-            ((LivingEntity) target).addStatusEffect(new StatusEffectInstance(StatusEffects.LEVITATION, 20 * 1 * level, level + 1));
+        if (target instanceof LivingEntity) {
+            ((LivingEntity) target).addStatusEffect(new StatusEffectInstance(StatusEffects.LEVITATION, 20 * 2 * level, 1));
         }
 
         super.onTargetDamaged(user, target, level);
     }
-
 }
